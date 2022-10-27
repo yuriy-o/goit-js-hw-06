49bb5e7ecc28acd6ebab14bc4e5d9ca63b19e230
Yuriy <sharik.kiev@gmail.com>
2022-10-27 23:51:55 +0300

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+
+const itemsRef = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  console.log(itemEl);
+
+  return itemEl;
+});
+
+list.append(...itemsRef);
