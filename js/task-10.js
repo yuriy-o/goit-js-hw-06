6f6641bfd8d09702c4e95ca347ddaeefd446b263
Yuriy <sharik.kiev@gmail.com>
2022-10-30 23:37:36 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,46 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const size = 30;
+
+const divControlEl = document.getElementById('controls');
+const inputEl = document.querySelector('input[type="number"]');
+
+const buttonCreateEl = document.querySelector('button[data-create]');
+const buttonDestroyEl = document.querySelector('button[data-destroy]');
+
+const divEl = document.getElementById('boxes');
+
+buttonCreateEl.addEventListener('click', onDivCreate);
+
+function onDivCreate() {
+  const arr = [];
+
+  for (let i = 0; i < inputEl.value; i += 1) {
+    const divNewEl = document.createElement('div');
+
+    divNewEl.classList.add('colorBox');
+    divNewEl.style.width = `${size + 10 * i}px`;
+    divNewEl.style.height = `${size + 10 * i}px`;
+    divNewEl.style.backgroundColor = getRandomHexColor();
+
+    arr.push(divNewEl);
+  }
+
+  divEl.append(...arr);
+}
+
+buttonDestroyEl.addEventListener('click', onDivRemove);
+
+function onDivRemove() {
+  const divNewEl333 = document.querySelectorAll('.colorBox');
+
+  divNewEl333.forEach(element => element.remove());
 }
