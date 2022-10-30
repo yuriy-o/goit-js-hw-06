8cee81447751b29208b5a31d1edec59c491f632d
Yuriy <sharik.kiev@gmail.com>
2022-10-31 00:36:25 +0200

    Additions to the code task 08

--- js/task-08.js
+++ js/task-08.js
@@ -17,13 +17,19 @@ function onFormSubmit(event) {
   //     alert('Введіть пароль');
   //     return;
   //   }
-  //Умова обов'язкових полів через один if
+
+  //Умова для обов'язкових полів через один if
   if (email.value === '' || password.value === '') {
     alert('Всі поля повинні бути заповнені');
     return;
   }
 
   // v1 -------------Виведення данних на сторінку-----------------
+  const hr = document.createElement('hr');
+  hr.style.width = '100%';
+  const hr2 = document.createElement('hr');
+  hr2.style.width = '100%';
+
   const titleEl = document.createElement('h3');
   titleEl.textContent = 'Були введені такі дані:';
 
@@ -35,24 +41,30 @@ function onFormSubmit(event) {
 
   const commentEl = document.createElement('p');
   commentEl.textContent =
-    'p.s. Спочатку вивів дані на сторінку, а потім дочитав умову завдання та додав виведення об`єкта введених даних у консоль.';
+    'p.s. Спочатку вивів дані на сторінку, а потім дочитав умову завдання )))) та додав виведення об`єкта введених даних у консоль.';
 
   const comment2El = document.createElement('p');
   comment2El.innerHTML =
-    'p.p.s. Щось не зрозумів як за допомогою <em><b>formData</b></em> можна вивести данні в консоль. Чи цей метод використовується не для цих витівок?';
+    'p.p.s. Щось не зрозумів як за допомогою <span style="color: red; font-size: 20px"><b >formData</b></span> або <span style="color: red; font-size: 20px"><b>elem.attributes</b></span> можна вивести данні в консоль.<br/>Чи цей метод використовується не для цих витівок?<br/>Якщо так, прошу розказати як це можна зробити ))';
 
-  formEl.append(titleEl, emailEl, passwordEl, commentEl, comment2El);
+  formEl.append(titleEl, emailEl, passwordEl, hr, commentEl, hr2, comment2El);
 
   // v2 -----------Виведення данних в консоль----------------
   console.log({ email: email.value, password: password.value });
 
-  //     v3 ----------------- ?? formData ?? -------------
-  //   const formData = new FormData(event.currentTarget);
+  // //v3 ----------------- ?? formData ?? -------------
+  // const formData = new FormData(event.currentTarget);
 
-  //   formData.forEach((value, name) => {
-  //     console.log({ name, value });
-  //   });
-  //      v3 ----------------- ?? formData ?? -------------
+  // formData.forEach((value, name) => {
+  //   console.log({ name, value });
+  // });
+  // //v3 ----------------- ?? formData ?? -------------
+
+  // //v4 ----------------- ?? elem.attributes ?? -------------
+  //Як вивести об'єкт атрибутів через elem.attributes ??
+  // //v4 ----------------- ?? elem.attributes ?? -------------
 
   event.currentTarget.reset();
 }
+
+//elem.attributes - властивість, що повертає об'єкт усіх атрибутів елемента.
