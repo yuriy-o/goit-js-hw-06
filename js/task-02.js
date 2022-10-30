3ae4b2d0435c56e2e209f177b8b568978c536858
Yuriy <sharik.kiev@gmail.com>
2022-10-31 00:47:53 +0200

    removes the extra console

--- js/task-02.js
+++ js/task-02.js
@@ -6,7 +6,7 @@ const itemsRef = ingredients.map(ingredient => {
   const itemEl = document.createElement('li');
   itemEl.textContent = ingredient;
   itemEl.classList.add('item');
-  console.log(itemEl);
+  // console.log(itemEl);
 
   return itemEl;
 });
