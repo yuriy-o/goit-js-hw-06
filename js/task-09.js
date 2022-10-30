ca2e5b296d2642bd705f5d0bb2320e77514427b5
Yuriy <sharik.kiev@gmail.com>
2022-10-30 14:38:53 +0200

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+//--------Функція була в коді за замовченням--------
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+//--------Функція з завдання LMS--------
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const onButtonClick = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+onButtonClick.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  const bodyEl = document.querySelector('body');
+  bodyEl.style.backgroundColor = getRandomHexColor();
+
+  spanEl.textContent = getRandomHexColor();
 }
